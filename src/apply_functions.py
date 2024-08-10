6ccd7dc2636c0ed1e9af258620760c34576a3837
Eric Zhang <ericspring08@gmail.com>
2024-08-10 17:29:15 -0500

    columns and rows with counts

--- src/apply_functions.py
+++ src/apply_functions.py
@@ -1,28 +1,46 @@
 import matplotlib.pyplot as plt
+import math
 
-def apply_flexDirection(figure: plt.Figure, flexDirection: str):
-    if flexDirection == "row":
+def apply_direction_count(figure: plt.Figure, direction: str, count: int):
+    if direction == "row":
         total_width = figure.get_figwidth()
         total_height = figure.get_figheight()
-    
+
+        # count is the number in each column for row direction
+        columns = math.ceil(len(figure.axes) / count)
+
         # calculate the width of each plot
-        width = total_width / len(figure.axes)
-        height = total_height
+        plot_height = total_height / count 
+        plot_width = total_width / columns
+
         for i, ax in enumerate(figure.axes):
-            ax.set_position([i * width / total_width, 0, width / total_width, height / total_height])
-    elif flexDirection == "column":
+            x = (i // count) * plot_width
+            y = total_height - ((i % count) * plot_height)
+
+            ax.set_position([x, y, plot_width, plot_height])
+
+    elif direction == "column":
         total_width = figure.get_figwidth()
         total_height = figure.get_figheight()
-    
+
+
+        # count is the number in each row for column direction
+        rows = math.ceil(len(figure.axes) / count)
+
         # calculate the height of each plot
-        width = total_width
-        height = total_height / len(figure.axes)
+        plot_height = total_height / rows
+        plot_width = total_width / count
+
         for i, ax in enumerate(figure.axes):
-            ax.set_position([0, i * height / total_height, width / total_width, height / total_height])
+            x = (i % count) * plot_width
+            y = total_height - ((i // count) * plot_height)
+
+            ax.set_position([x, y, plot_width, plot_height])
     else:
         return ValueError("Flex direction must be either row or column")
 
     return figure
+
 
 def apply_margin(figure: plt.Figure, margin: int):
     for ax in figure.axes:
@@ -32,8 +50,8 @@ def apply_margin(figure: plt.Figure, margin: int):
         # calculate the new position
         x = x + margin
         y = y + margin
-        width = width - 2 * margin
-        height = height - 2 * margin
+        width = width - margin
+        height = height - margin
         ax.set_position([x, y, width, height])
 
     return figure
---
+++ src/autolayout.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from src.apply_functions import apply_direction_count, apply_margin
+
+
+def autolayout(figure: plt.Figure, configuration=None):
+    if configuration is None:
+        return ValueError("Flexbox config is required")
+
+    figure = apply_direction_count(figure, configuration["direction"], configuration["count"])
+    figure = apply_margin(figure, configuration["margin"])
+
+    return figure
