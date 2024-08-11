ef56ac5b8264172293ce682b3a2c2027aab5d801
Eric Zhang <ericspring08@gmail.com>
2024-08-10 23:56:04 -0500

    first publish

--- matplotlib_autolayout/autolayout.py
+++ matplotlib_autolayout/autolayout.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
-from src.apply_functions import apply_direction_count, apply_margin
+from matplotlib_autolayout.apply_functions import apply_direction_count, apply_margin
 
 
 def autolayout(figure: plt.Figure, configuration=None):
     if configuration is None:
         return ValueError("Flexbox config is required")
 
-    figure = apply_direction_count(figure, configuration["direction"], configuration["count"])
+    figure = apply_direction_count(
+        figure, configuration["direction"], configuration["count"])
     figure = apply_margin(figure, configuration["margin"])
 
     return figure
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="matplotlib-autolayout",
-    version="0.0.1",
+    version="0.0.3",
     author="Eric Zhang",
     author_email="ericspring08@gmail.com",
     description="Auto layouts for matplotlib multiplots",
@@ -13,5 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ericspring08/matplotlib-autolayout",
     python_requires=">=3.6",
-    packages=setuptools.find_packages(),
+    packages=['matplotlib_autolayout'],
+    install_requires=[
+        'matplotlib',
+        'numpy',
+    ],
 )
